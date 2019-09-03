c5ef684b99bd5f0a2af547ddf3224ae2e64bfc4b
Oni Farabi <52005015+Oni-Farabi@users.noreply.github.com>
2019-09-03 10:40:05 +0600

    Add files via upload

--- graph-project/main.cpp
+++ graph-project/main.cpp
@@ -3,16 +3,16 @@
 #include <limits.h>
 using namespace std;
 
-// Class for an undirected graph
+
 class Graph
 {
-    int Y;    // No. of vertices
-    list<int> *arr; // Pointer to an array for array lists
+    int Y;
+    list<int> *arr;
     bool isCyclicUtil(int v, bool visited[], int parent);
 public:
-    Graph(int Y);   // Constructor
-    void addEdge(int v, int w);   // to add an edge to graph
-    bool isTree();   // returns true if graph is tree
+    Graph(int Y);
+    void addEdge(int v, int w);
+    bool isTree();
 };
 
 Graph::Graph(int Y)
@@ -23,54 +23,46 @@ Graph::Graph(int Y)
 
 void Graph::addEdge(int v, int w)
 {
-    arr[v].push_back(w); // Add w to v’s list.
-    arr[w].push_back(v); // Add v to w’s list.
+    arr[v].push_back(w);
+    arr[w].push_back(v);
 }
 
-// A recursive function that uses visited[] and parent to
-// detect cycle in subgraph reachable from vertex v.
 bool Graph::isCyclicUtil(int v, bool visited[], int parent)
 {
-    // Mark the current node as visited
+
     visited[v] = true;
 
-    // Recur for all the vertices adjacent to this vertex
+
     list<int>::iterator i;
     for (i = arr[v].begin(); i != arr[v].end(); ++i)
     {
-        // If an array is not visited, then recur for
-        // that array
+
         if (!visited[*i])
         {
            if (isCyclicUtil(*i, visited, v))
               return true;
         }
 
-        // If an array is visited and not parent of current
-        // vertex, then there is a cycle.
+
         else if (*i != parent)
            return true;
     }
     return false;
 }
 
-// Returns true if the graph is a tree, else false.
+
 bool Graph::isTree()
 {
-    // Mark all the vertices as not visited and not part of
-    // recursion stack
+
     bool *visited = new bool[Y];
     for (int i = 0; i < Y; i++)
         visited[i] = false;
 
-    // It returns true if graph reachable from vertex 0
-    // is cyclcic. It also marks all vertices reachable
-    // from 0.
+
     if (isCyclicUtil(0, visited, -1))
              return false;
 
-    // If we find a vertex which is not reachable from 0
-    // (not marked by isCyclicUtil(), then we return false
+
     for (int u = 0; u < Y; u++)
         if (!visited[u])
            return false;
